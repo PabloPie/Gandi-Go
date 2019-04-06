e2e17a73c33fbcc7ffe8c27b68214bf44881af9e
Pablo <ppiedrafita95@gmail.com>
2019-04-06 15:37:51 +0200

    Add functions to wait for operations and to list regions

--- hosting/operation.go
+++ hosting/operation.go
@@ -1,11 +1,44 @@
 package hosting
 
+import (
+	"fmt"
+	"time"
+)
+
+type operationInfo struct {
+	ID     int    `xmlrpc:"id"`
+	Status string `xmlrpc:"step"`
+}
+
 type Operation struct {
-	DiskID  int    `xmlrpc:"disk_id"`
 	ID      int    `xmlrpc:"id"`
+	VMID    int    `xmlrpc:"vm_id"`
+	DiskID  int    `xmlrpc:"disk_id"`
 	IfaceID int    `xmlrpc:"iface_id"`
 	IPID    int    `xmlrpc:"ip_id"`
 	Step    string `xmlrpc:"step"`
 	Type    string `xmlrpc:"type"`
-	VMID    int    `xmlrpc:"vm_id"`
+}
+
+func (h Hostingv4) waitForOp(op Operation) error {
+	var res = operationInfo{}
+	params := []interface{}{op}
+	err := h.Send("operation.info", params, &res)
+	if err != nil {
+		return err
+	}
+	for res.Status != "DONE" {
+		time.Sleep(5 * time.Second)
+		err := h.Send("operation.info", params, &res)
+		if err != nil {
+			return err
+		}
+		if res.Status == "DONE" {
+			return nil
+		}
+		if res.Status != "BILL" && res.Status != "WAIT" && res.Status != "RUN" {
+			return fmt.Errorf("Bad operation status for %d : %s", op.ID, res.Status)
+		}
+	}
+	return nil
 }
--- hosting/region.go
+++ hosting/region.go
@@ -1,12 +1,45 @@
 package hosting
 
+import (
+	"errors"
+)
+
+// RegionManager represents a service capable of getting info
+// about Gandi Datacenters
 type RegionManager interface {
 	ListRegions() []Region
-	RegionbyCode() Region
+	RegionbyCode(code string) Region
 }
 
+// Region represents a Gandi datacenter
 type Region struct {
-	ID    int
-	Name  string
-	State int
+	ID      int    `xmlrpc:"id"`
+	Name    string `xmlrpc:"dc_code"`
+	Country string `xmlrpc:"country"`
+}
+
+// ListRegions lists every Gandi datacenter
+func (h Hostingv4) ListRegions() ([]Region, error) {
+	var res = []Region{}
+	request := []interface{}{}
+	err := h.Send("hosting.datacenter.list", request, &res)
+	if err != nil {
+		return []Region{}, err
+	}
+	return res, nil
+}
+
+// RegionbyCode returns the region with code `code` if it exists
+func (h Hostingv4) RegionbyCode(code string) (Region, error) {
+	var res = []Region{}
+	var filter = map[string]string{"dc_code": code}
+	request := []interface{}{filter}
+	err := h.Send("hosting.datacenter.list", request, &res)
+	if err != nil {
+		return Region{}, err
+	}
+	if len(res) != 1 {
+		return Region{}, errors.New("Region not found")
+	}
+	return res[0], nil
 }
