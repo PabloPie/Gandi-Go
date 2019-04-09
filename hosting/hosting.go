045907ff410a6cf5d02bdf152d3b6466c4ae2820
Pablo <ppiedrafita95@gmail.com>
2019-04-09 09:06:55 +0200

    structToMap function is common to all hosting operations

--- hosting/hosting.go
+++ hosting/hosting.go
@@ -1,6 +1,9 @@
 package hosting
 
 import (
+	"errors"
+	"reflect"
+
 	"github.com/PabloPie/Gandi-Go/client"
 )
 
@@ -22,4 +25,27 @@ type Hostingv4 struct {
 
 func Newv4Hosting(client client.V4Caller) Hosting {
 	return Hostingv4{client}
+}
+
+// structToMap writes every tagged(with key "xmlrpc"), non Zero Value
+// from the struct into a map
+func structToMap(s interface{}) (map[string]interface{}, error) {
+	v := reflect.ValueOf(s)
+	if v.Kind() != reflect.Struct {
+		return nil, errors.New("Not a struct")
+	}
+
+	out := map[string]interface{}{}
+	t := v.Type()
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+		name := field.Tag.Get("xmlrpc")
+		value := v.Field(i).Interface()
+		zeroval := reflect.Zero(field.Type).Interface()
+		if reflect.DeepEqual(value, zeroval) || name == "" {
+			continue
+		}
+		out[name] = value
+	}
+	return out, nil
 }
