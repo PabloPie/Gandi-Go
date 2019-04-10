d85373145b69359a3d332fc7baebd1d8feedc0b2
Pablo <ppiedrafita95@gmail.com>
2019-04-10 18:41:48 +0200

    Separate images from disks

---
+++ hosting/image.go
@@ -0,0 +1,18 @@
+package hosting
+
+// ImageManager represents a service capable of getting information about
+// Gandi Disk images
+type ImageManager interface {
+	ImageByName(name string, region Region) (DiskImage, error)
+	ListImagesInRegion(region Region) ([]DiskImage, error)
+}
+
+// DiskImage represents an image defined by Gandi
+// with an OS, used to create new Disks and VMs
+type DiskImage struct {
+	ID       string
+	DiskID   string
+	RegionID string
+	Name     string
+	Size     uint
+}
